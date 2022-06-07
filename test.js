59780a3e23dff4a433994c9c854c3dbcc7edfae6
muldaeguy <muldaeguy@gmail.com>
2022-06-08 04:35:32 +0900

    영상 넣기 시도..

---
+++ test.js
@@ -0,0 +1,30 @@
+window.onload = function () {
+  const parts = [];
+  let mediaRecorder;
+  navigator.mediaDevices.getUserMedia({ audio: true, video: true }).then(stream => {
+    document.getElementByID("video").srcObject = stream;
+    document.getElementByID("btn").onclick = function () {
+      mediaRecorder = new MediaRecorder(stream);
+
+      mediaRecorder.start(1000);
+
+      mediaRecorder.ondataavailable = function (e) {
+          part.push(e.data);
+      }
+    }
+  });
+
+  document.getElementByID("stopbtn").onclick = function () {
+      mediaRecorder.stop();
+      const blob = new Blob(parts, {
+        type: "video/webm"
+      });
+      const url = URL.createObjectURL(blob);
+      const a = document.createElement("a");
+      document.body.appendChild(a);
+      a.style = "display: none";
+      a.href = url;
+      a.download = "test.webm";
+      a.click();
+  }
+}
